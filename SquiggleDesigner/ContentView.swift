00867ae90fef58f9030fc9d96e314a1c3da7e7b9
Christoph Brass <mail@cbrass.de>
2024-01-29 21:36:03 +0100

    Initial Commit

---
+++ SquiggleDesigner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SquiggleDesigner
+//
+//  Created by Christoph Braß on 29.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SquiggleDesigner/SquiggleDesignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquiggleDesignerApp.swift
+//  SquiggleDesigner
+//
+//  Created by Christoph Braß on 29.01.24.
+//
+
+import SwiftUI
+
+@main
+struct SquiggleDesignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
